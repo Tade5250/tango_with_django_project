fdae63aca476aed25f590d68de085eb1f3eacbb1
“Temiloluwa <2619191a@student.gla.ac.ukgit config --global user.email 2619191a@student.gla.ac.uk>
2023-01-23 23:09:42 +0000

    Chapter 3 and Chapter 4

---
+++ rango/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+    #return HttpResponse("Rango says hey there partner! <a href='/rango/about/'> About </a>")
+
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Temi Adeola'}
+    return render(request, 'rango/about.html', context = context_dict)
